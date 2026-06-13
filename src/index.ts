47813e2f9d3c2a727cb43291ce836d200c7f2463
maitatduy <maitatduy.work@gmail.com>
2026-06-13 20:54:24 +0700

    feat: update CORS configuration to restrict allowed origins

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
     cors({
-        origin: [
-            "http://localhost:5500",
-            "http://127.0.0.1:5500",
-            "http://localhost:5501",
-            "http://127.0.0.1:5501",
-        ],
+        origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
